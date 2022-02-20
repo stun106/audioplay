b1d31e90edb88f87f8bf89572ad4af34f93d29ae
Jose Antonio <bladingnordeste@gmail.com>
2022-02-20 19:40:42 -0300

    reprodutor de audio
    estudo baseado em html, css e mto JS

---
+++ script.js
@@ -0,0 +1,51 @@
+var musica = document.querySelector('audio');
+
+//Eventos
+
+ document.querySelector('.playbtn').addEventListener('click', tocarMusica);
+
+ document.querySelector('.pausebtn'). addEventListener('click', pauseMusica);   
+
+ document.querySelector('.stopbtn'). addEventListener('click', pararMusica);
+
+ musica.addEventListener('timeupdate', atualizarBarra);
+
+ var duracaoMusica = document.querySelector('.fim');
+ duracaoMusica.textContent = segundosParaMinutos(Math.floor(musica.duration)); 
+    
+    
+  //funções  
+    
+ function tocarMusica(){
+    musica.play();
+}
+
+
+function pauseMusica(){
+    musica.pause();
+}
+
+
+function pararMusica(){
+    musica.pause();
+    musica.currentTime = 0;
+
+}
+
+function atualizarBarra(){
+   var barra = document.querySelector('progress');
+   barra.style.width = (musica.currentTime / musica.duration)* 100 + '%';
+   var tempoDecorrido = document.querySelector('.inicio');
+   tempoDecorrido.textContent = segundosParaMinutos (Math.floor(musica.currentTime));
+}
+
+function segundosParaMinutos(segundos){
+    var campoMinuto = Math.floor(segundos / 60);
+    var campoSegundos = segundos % 60;
+    if (campoSegundos < 10){
+        campoSegundos = '0 ' + campoSegundos 
+    } 
+    return campoMinuto+':'+campoSegundos;
+       
+    
+}
